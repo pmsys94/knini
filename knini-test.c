7798ee593d889fd902950f8405c99a814d446bc3
Pierre Meißner <kperia.inet@gmx.de>
2018-02-11 17:12:27 +0100

    added comdline funtion

--- knini-test.c
+++ knini-test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "testfuncs.h"
 
 void showMenu();
@@ -14,15 +15,27 @@ int main(int argc, char* argv[]){
 	fprintf(stderr, "[DEBUG] Argv[0]= %s\n", argv[0]);
 #endif
 	if (argc > 1){ // cmd arg init
-		if(argc > 2){
+		if(argc == 2){
+			fprintf(stderr, "To less arguments!\n");
+			cmdUsage();
+			return 1;
+		} else if(argc > 3){
 			fprintf(stderr, "To many arguments!\n");
 			cmdUsage();
 			return 1;
 		} else {
-			inifile = argv[1];
+			inifile = argv[2];
 #ifdef DEBUG
-			fprintf(stderr, "[DEBUG] argv[1] = %s\ninifile = %s\n", argv[1], inifile);
+			fprintf(stderr, "[DEBUG] argv[1] = %s\n", argv[1]);
+			fprintf(stderr, "[DEBUG] argv[2] = %s\n-> inifile = %s\n", argv[2], inifile);
 #endif
+			if(strcmp(argv[1], "psections\0") == 0){
+				printSections(inifile);
+			} else {
+				fprintf(stderr, "Paramater 1 '%s' was not a valid action!\n", argv[1]);
+				cmdUsage();
+				return 1;
+			}
 		}
 	} else { // normal testprogram init
 		printf("Ini file parser test application\n");
@@ -71,7 +84,8 @@ void showMenu(){
 
 void cmdUsage(){
 	fprintf(stderr, "Usage of testing application:\n"
-			"<app> <ini-file>\n"
-			"e.g. <app> mytest.ini\n"
+			"<app> <action> <ini-file>\n"
+			"Actions are: psections;\n"
+			"e.g. <app> psections mytest.ini\n"
 	);
 }
